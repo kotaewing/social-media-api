dff6d71c141698db9eeb41ffbf88688374ca7cb3
kotaewing <kota.ewing@gmail.com>
2021-07-09 15:07:58 -0600

    add and remove friends

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -44,6 +44,26 @@ const userController = {
         )
             .then(dbUserData => res.json(dbUserData))
             .catch(err => res.json(err))
+    },
+
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $push: { friends: params.friendId } },
+            { new: true }
+        )
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err))
+    },
+
+    removeFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId } },
+            { new: true }
+        )
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err))
     }
 
 }
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { getAllUsers, getUserById, createUser, updateUser, deleteUser } = require('./../controllers/user-controller');
+const { getAllUsers, getUserById, createUser, updateUser, deleteUser, addFriend, removeFriend } = require('./../controllers/user-controller');
 
 router
     .route('/')
@@ -10,6 +10,12 @@ router
     .route('/:userId')
     .get(getUserById)
     .put(updateUser)
-    .delete(deleteUser)
+    .delete(deleteUser);
+
+router
+    .route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(removeFriend);
+
 
 module.exports = router;
